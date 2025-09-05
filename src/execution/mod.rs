66f19ef4d30120d44ef681fb63392dcd0872e590
Toikid23 <183738110+Toikid23@users.noreply.github.com>
2025-09-06 00:25:52 +0200

    update

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -4,4 +4,7 @@
 pub mod optimizer;        // Pour calculer le montant optimal d'un trade.
 pub mod simulate;         // Pour simuler les transactions avant envoi.
 pub mod bundle_builder;   // Pour construire les bundles Jito.
-pub mod bundle_sender;    // Pour envoyer les bundles aux Block Engines.
+pub mod bundle_sender;
+pub mod protection;
+pub mod simulator;
+// Pour envoyer les bundles aux Block Engines.
